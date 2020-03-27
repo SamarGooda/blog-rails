e5f6599734f5165e20871fd30513adc1666fc7b0
samar <samar@localhost.localdomain>
2020-03-27 23:34:56 +0200

    create & edit &delete comment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,6 @@ class PostsController < ApplicationController
   end
   def create
     @post = Post.new( post_params)
-
     if @post.save
       redirect_to @post
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :comments
   #defult if I write this linke it will search about controller with the same name
   get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
