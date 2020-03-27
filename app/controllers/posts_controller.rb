6e9959dfd01618436d6c1cdcd41e95166c2d6150
samar <samar@localhost.localdomain>
2020-03-27 16:40:52 +0200

    add delete & edit method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,113 @@
 class PostsController < ApplicationController
-
   def index
+    @posts = Post.all
   end
-
   def create
-  end
+    @post = Post.new( post_params)
 
+    @post.save
+    redirect_to @post
+  end
   def new
+    @post=Post.new
   end
-
   def edit
-  end
+    @post = Post.find(params[:id])
+    if @post.edit
 
+    end
+  end
   def update
   end
-
   def show
+    @post = Post.find(params[:id])
+
+  end
+  def destroy
+    @post = Post.find(params[:id])
+    if @post.destroy
+      redirect_to posts_path
+    end
   end
 
-  def destroy
-  end
 end
+private
+def post_params
+  params.require(:post).permit(:title, :text)
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class PostsController < ApplicationController
+#
+#   def index
+#     @posts =Post.all
+#   end
+#
+#   def create
+#     #render [:plain =>params [:post].inspect]
+#     # render plain: params[:post] .inspect
+#
+#      @post = Post.new(post_params)
+#     #to save data in database
+#      @post.save
+#     #to path to show
+#     redirect_to @post
+#   end
+#
+#   def new
+#     # to make an object from post
+#     @post=Post.new
+#   end
+#
+#   def edit
+#   end
+#
+#   def update
+#   end
+#
+#   def show
+#     @post = Post.find(params[:id])
+#   end
+#
+#   def destroy
+#   end
+# end
+#
+# private
+# def post_params
+#   params.require(:post).permit(:title, :text)
+# end
+#
+#
+#
+#
